c300ea9f4c1656640dd5fca1187cfe9a4c3d35ea
Stuart Carruthers <stu@ironiclensflare.com>
2019-10-18 23:34:07 +0100

    Testing RTT API

--- trains.go
+++ trains.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Getting trains...")
+	getTrains()
+}
+
+const URL = "https://api.rtt.io/api/v1/json/search/BMH"
+
+func getTrains() {
+	req, err := http.NewRequest("GET", URL, nil)
+	req.SetBasicAuth(os.Getenv("RTTUSER"), os.Getenv("RTTPASS"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
