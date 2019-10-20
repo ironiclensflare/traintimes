afb913c8f085e2c58f5fc608dc7accf7a8747ab5
Stuart Carruthers <stu@ironiclensflare.com>
2019-10-20 23:19:31 +0100

    Mapped remaining fields

--- main.go
+++ main.go
@@ -20,5 +20,8 @@ func getTrains() {
 	trainsJSON, _ := service.GetTrainsDepartingFrom(args[0])
 	var trains models.DepartureBoard
 	json.Unmarshal([]byte(trainsJSON), &trains)
-	fmt.Println(trains.Location.Name)
+
+	for _, s := range trains.Services {
+		fmt.Printf("%v - %v (%v)\n", s.LocationDetail.RealtimeDeparture, s.LocationDetail.Destination[0].Description, s.ATOCName)
+	}
 }
--- models/departureboard.go
+++ models/departureboard.go
@@ -3,6 +3,7 @@ package models
 // DepartureBoard represents a departure board
 type DepartureBoard struct {
 	Location Location
+	Services []Service
 }
 
 // Location represents a location
@@ -10,4 +11,43 @@ type Location struct {
 	Name   string
 	CRS    string
 	TIPLOC string
+}
+
+// Service represents a train service
+type Service struct {
+	LocationDetail  LocationDetail
+	ServiceUID      string
+	RunDate         string
+	TrainIdentity   string
+	RunningIdentity string
+	ATOCCode        string
+	ATOCName        string
+	ServiceType     string
+	IsPassenger     bool
+	Origin          []Pair
+	Destination     []Pair
+}
+
+// LocationDetail represents a location
+type LocationDetail struct {
+	Origin                  []Pair
+	Destination             []Pair
+	IsCall                  bool
+	IsPublicCall            bool
+	RealtimeArrival         string
+	RealtimeArrivalActual   bool
+	RealtimeDeparture       string
+	RealtimeDepartureActual bool
+	Platform                string
+	PlatformConfirmed       bool
+	PlatformChanged         bool
+	DisplayAs               bool
+}
+
+// Pair represents an origin or destination station
+type Pair struct {
+	TIPLOC      string
+	Description string
+	WorkingTime string
+	PublicTime  string
 }
